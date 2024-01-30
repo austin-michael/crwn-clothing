de28f9e14cc6a7928b207f081eb89eb89a856922
austin-michael <austin.michaelwill@gmail.com>
2024-01-30 11:45:07 -0700

    Typing Category Actions

--- src/store/categories/category.types.ts
+++ src/store/categories/category.types.ts
@@ -3,3 +3,16 @@ export enum CATEGORIES_ACTION_TYPES {
   FETCH_CATEGOIES_SUCCESS = "category/FETCH_CATEGOIES_SUCCESS",
   FETCH_CATEGOIES_FAILED = "category/FETCH_CATEGOIES_FAILED",
 }
+
+export type CategoryItem = {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+};
+
+export type Category = {
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[];
+};
