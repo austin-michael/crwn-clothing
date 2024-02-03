b1c3b523c84c449cba3cf741598e437d69211c4f
austin-michael <austin.michaelwill@gmail.com>
2024-02-03 16:13:14 -0700

    Mobile styling

--- src/components/directory/directory.styles.tsx
+++ src/components/directory/directory.styles.tsx
@@ -1,8 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const DirectoryContainer = styled.div`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+
+  @media screen and (max-width: 800px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 600px) {
+    grid-template-columns: 1fr;
+  }
 `;
--- src/routes/category/category.styles.tsx
+++ src/routes/category/category.styles.tsx
@@ -1,10 +1,19 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CategoryContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
   row-gap: 50px;
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+    row-gap: 25px;
+  }
+
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const Title = styled.h2`
