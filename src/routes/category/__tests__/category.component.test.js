4d53ed40204da51c1986af407207b9fcb759e307
austin-michael <austin.michaelwill@gmail.com>
2024-02-06 16:31:32 -0700

    Added Category tests

---
+++ src/routes/category/__tests__/category.component.test.js
@@ -0,0 +1,55 @@
+import { screen } from "@testing-library/react";
+
+import Category from "../category.component";
+import { renderWithProviders } from "../../../utils/test/test.utils";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({
+    category: "mens",
+  }),
+}));
+
+describe("Category tests", () => {
+  test("should render a spinner if isLoading is true", () => {
+    renderWithProviders(<Category />, {
+      preloadedState: {
+        categories: {
+          isLoading: true,
+          categories: [],
+        },
+      },
+    });
+
+    const spinnerElement = screen.getByTestId("spinner");
+    expect(spinnerElement).toBeInTheDocument();
+  });
+
+  test("should render categories and items if isLoading is false", () => {
+    renderWithProviders(<Category />, {
+      preloadedState: {
+        categories: {
+          isLoading: false,
+          categories: [
+            {
+              title: "mens",
+              items: [
+                { id: 1, name: "product 1" },
+                { id: 2, name: "product 2" },
+              ],
+            },
+          ],
+        },
+      },
+    });
+
+    const spinnerElement = screen.queryByTestId("spinner");
+    expect(spinnerElement).toBeNull();
+
+    const product1Element = screen.getByText(/product 1/i);
+    expect(product1Element).toBeInTheDocument();
+
+    const product2Element = screen.getByText(/product 2/i);
+    expect(product2Element).toBeInTheDocument();
+  });
+});
